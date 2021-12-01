adc1098069823b0219e37e07d129de4bffbdafbf
James Walker <james.chunho+github@gmail.com>
2021-12-01 01:57:21 +0000

    Better error message

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local module = {}
 
 
 function module.lists(a, b)
-  assert(#a == #b, "Tables have different length!")
+  assert(#a == #b, ("Tables have different length: %s ~= %s"):format(#a, #b))
   for i=1,#a do
     assert(a[i] == b[i], ("Tables have different content: %s ~= %s"):format(a[i], b[i]))
   end
