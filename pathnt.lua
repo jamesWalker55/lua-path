d8f6d21ada75f71bd7b052dc5d346ea3f25936c4
James Walker <james.chunho+github@gmail.com>
2021-12-01 01:20:17 +0000

    Implement a bunch of functions

--- pathnt.lua
+++ pathnt.lua
@@ -86,6 +86,101 @@ function module.join(path, ...)
   return result_drive .. result_path
 end
 
+
+
+-- Normalize a path, e.g. A//B, A/./B and A/foo/../B all become A\B.
+-- Previously, this function also truncated pathnames to 8+3 format,
+-- but as this module is called "ntpath", that's obviously wrong!
+
+--- Normalize path, eliminating double slashes, etc.
+function module.normpath(path)
+  if contains(path:sub(1, 4), {[[\\.\]], [[\\?\]]}) then
+    -- in the case of paths with these prefixes:
+    -- \\.\ -> device names
+    -- \\?\ -> literal paths
+    -- do not do any normalization, but return the path
+    -- unchanged apart from the call to os.fspath()
+    return path
+  end
+
+  path = path:gsub("/", "\\")
+  local prefix, path = module.splitdrive(path)
+
+  -- collapse initial backslashes
+  if path:sub(1,1) == "\\" then
+    prefix = prefix .. "\\"
+
+    path = ({path:gsub("^\\+", "")})[1]
+  end
+
+  -- split by separator into list
+  local comps = util.split(path, "[^\\\\]*")
+  local i = 1
+  while i <= #comps do
+    if #comps[i] == 0 or comps[i] == "." then
+      table.remove(comps, i)
+    elseif comps[i] == ".." then
+      if i > 1 and comps[i-1] ~= ".." then
+        for _ = 0, 1 do -- do twice
+          table.remove(comps, i - 1)
+        end
+        i = i - 1
+      elseif i == 1 and prefix:sub(-1, -1) == "\\" then
+        table.remove(comps, i)
+      else
+        i = i + 1
+      end
+    else
+      i = i + 1
+    end
+  end
+  -- If the path is now empty, substitute '.'
+  if #prefix == 0 and #comps == 0 then
+    comps[#comps + 1] = "."
+  end
+  return prefix .. table.concat(comps, "\\")
+end
+
+
+--- Test whether a path is absolute
+--- @param path string
+function module.isabs(path)
+  -- Paths beginning with \\?\ are always absolute, but do not necessarily contain a drive.
+  local ABS_PATTERN = [[\\?\]]
+  if path:gsub('/', '\\'):sub(1, 4) == ABS_PATTERN then return true end
+
+  _, path = module.splitdrive(path)
+  return #path > 0 and contains(path:sub(1, 1), {"/", "\\"})
+end
+
+
+--- Get the current working directory
+--- There is no native function to do this, you MUST use an external library for this
+function module.getcwd()
+  assert(pandoc ~= nil, "#getcwd is only supported in a Pandoc environment")
+  return pandoc.system.get_working_directory()
+end
+
+--- Return an absolute path.
+--- @param path string
+function module.abspath(path)
+  if not module.isabs(path) then
+    local cwd = module.getcwd()
+    path = module.join(cwd, path)
+  end
+  return module.normpath(path)
+end
+
 print(module.join("c:/windows", "test", "hi"))
+print(module.isabs("c:/windows"))
+print(module.isabs("c:windows"))
+print(debug.getinfo(1).short_src)
+print(debug.getinfo(1).source)
+print(module.normpath([[A//B]]))
+print(module.normpath([[A/./B]]))
+print(module.normpath([[A/foo/../B]]))
+print(module.normpath([[A\B]]))
+print(module.abspath([[A\B/../..]]))
+print(module.abspath([[A\B/../../..]]))
 
 return module
