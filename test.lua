96cf527d0b4309fccf34fabd73bcdd29e2628552
James Walker <james.chunho+github@gmail.com>
2021-12-02 22:17:40 +0000

    formatter

--- test.lua
+++ test.lua
@@ -1,22 +1,18 @@
 local module = {}
 
-
 function module.bool(a, b)
   assert(a == b, ("Bools are different: %s ~= %s"):format(a, b))
 end
-
 
 function module.strings(a, b)
   assert(a == b, ("Strings are different: '%s' ~= '%s'"):format(a, b))
 end
 
-
 function module.lists(a, b)
   assert(#a == #b, ("Tables have different length: %s ~= %s"):format(#a, #b))
-  for i=1,#a do
+  for i = 1, #a do
     assert(a[i] == b[i], ("Tables have different content: %s ~= %s"):format(a[i], b[i]))
   end
 end
-
 
 return module
